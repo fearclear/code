53d6a0ac3b0cde4b1fecd9caeae5ba76c557a5e4
fearclear <fearcleari@gmail.com>
2017-12-06 11:07:15 +0800

    回退处理

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,20 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';
-import Data from "./data.xml";
 function component() {
     let ele = document.createElement('div');
-    let ele2 = document.createElement('div');
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    ele2.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    ele2.classList.add('hello');
-    ele.appendChild(ele2);
+    ele.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    const myIcon = new Image();
-    myIcon.src = Icon;
-    ele.appendChild(myIcon);
-
-    console.log(Data)
     return ele;
 }
 document.body.appendChild(component())
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -5,40 +5,5 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|gif|jpg|svg)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.xml$/,
-                use: [
-                    'xml-loader'
-                ]
-            },
-            {
-                test: /\.(csv|tsv)$/,
-                use: [
-                    'csv-loader'
-                ]
-            }
-        ]
     }
 }
