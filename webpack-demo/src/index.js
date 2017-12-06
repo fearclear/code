121271884415f853bc8cf44f24116ff47a68e61b
fearclear <fearcleari@gmail.com>
2017-12-06 17:42:57 +0800

    output & plugin

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,9 +1,14 @@
 import _ from 'lodash';
+import printMe from './print.js';
 function component() {
     let ele = document.createElement('div');
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     ele.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    const btn = document.createElement('button');
 
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    ele.appendChild(btn);
     return ele;
 }
 document.body.appendChild(component())
---
+++ webpack-demo/src/print.js
@@ -0,0 +1,3 @@
+export default function printMe() {
+    console.log('Iget called from print.js!');
+}
