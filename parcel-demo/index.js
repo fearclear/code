cecb5d9c477f48d1c70e0f618e52303930094657
fearclear <fearcleari@gmail.com>
2017-12-22 15:55:33 +0800

    imp ts for project

--- parcel-demo/index.js
+++ parcel-demo/index.js
@@ -1,8 +1,10 @@
 import info from './info';
 import Api from './api';
+import Hero from './hero';
 
 
 let api = new Api();
 console.log(api);
 api?api.warning():'';
 console.log('%c hello parcel! ', 'background: yellowgreen; color: #fff');
+console.log(Hero);
