161d778b9a9f11d6c8efa4f72d02af3164c7d0fc
fearclear <fearcleari@gmail.com>
2018-01-17 17:44:14 +0800

    little

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
+router.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, accept, origin, content-type')
+  res.header('Access-Control-Request-Headers', '*')
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+  res.header('Content-Type', 'application/json;charset=utf-8')
+  next()
+})
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+module.exports = router
