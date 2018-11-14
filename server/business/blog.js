ef8c4fc5e46e676f5736983a9b16f580eba96e29
fearclear <fearcleari@gmail.com>
2018-11-14 20:20:35 +0800

    server已经由express转为egg开发，数据库从mongo改为mysql，同时迁移到单独项目

--- server/business/blog.js
+++
@@ -1,5 +0,0 @@
-/**
- * @namespace 博客文章
- * @description 博客列表及增删改查
- * @author fearclear <fearcleari@gmail.com>
- */
--- server/business/index.js
+++
@@ -1,7 +0,0 @@
-const user = require('./user')
-
-module.exports = {
-  signUp: user.signUp, // 注册
-  signIn: user.signIn, // 登录
-  checkUserName: user.checkUserName // 检查用户名合法性
-}
